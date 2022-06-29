119eba62112461cfd85101d0cd5bd54d765202a4
Bric_ih45 <brichka.p@gmail.com>
2022-06-29 20:50:26 +0300

    List

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,6 +7,12 @@ using System.Windows.Forms;
 namespace ConsoleApp4
 {
 
+	class Fish
+    {
+		string fish { get; set; }
+		int size { get; set; }
+
+    }
 	class Program
 	{
 		class Game
@@ -22,15 +28,15 @@ namespace ConsoleApp4
 				point = 0;
 				fish = new string[] { "small", "medium", "big" };
 			}
+
 			public void PlaceFish()
 			{
 				Random r = new Random();
+			
 				Console.WriteLine(fish[r.Next(fish.Length)] + " | " + fish[r.Next(fish.Length)] + " | ");
 			}
-				Console.WriteLine(fish[r.Next(fish.Length)]+ " | " + fish[r.Next(fish.Length)] + " | ");
-            }
+			//Console.WriteLine(fish[r.Next(fish.Length)]+ " | " + fish[r.Next(fish.Length)] + " | ");
 		}
-
 		static void Main(string[] args)
 		{
 			string vvod = "";
@@ -39,7 +45,11 @@ namespace ConsoleApp4
 			while (x != 0)
 			{
 				Console.WriteLine("0 - Exit");
+				Console.Write("Write menu__ ");
+				vvod = Console.ReadLine();
+				x = Convert.ToInt32(vvod);
 
+			
 
 
 				if (x == 0)
@@ -57,9 +67,12 @@ namespace ConsoleApp4
 				}
 			}
 		}
-
-
 	}
 
-
+	
 }
+
+
+
+
+
